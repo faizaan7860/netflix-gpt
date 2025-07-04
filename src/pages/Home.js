9564bd61666189944c7646437ae61173643b5c8a
Faizan <faizangandhi11@gmail.com>
2025-07-04 23:14:53 +0530

    Added all project files

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <h2>Home</h2>
+      <p>
+        Awesome! Here's your Weekly Advanced React Roadmap using the best
+        hand-picked resources from YouTube + Docs + Practice sites. It's paced
+        so you can deeply understand and apply what you learn.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
